3de721d3d4454a7412002cd9d1066aeb79241690
draygon23 <55451124+draygon23@users.noreply.github.com>
2020-02-05 15:41:16 -0500

    Add files via upload

---
+++ OpenCV.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # define range of blue color in HSV
+    lower_blue = np.array([90,110,90])
+    upper_blue = np.array([150,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange (hsv, lower_blue, upper_blue)
+    bluecnts = cv2.findContours(mask.copy(),
+                              cv2.RETR_EXTERNAL,
+                              cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+    if len(bluecnts)>0:
+        blue_area = max(bluecnts, key=cv2.contourArea)
+        (xg,yg,wg,hg) = cv2.boundingRect(blue_area)
+        cv2.rectangle(frame,(xg,yg),(xg+wg, yg+hg),(0,255,0),2)
+        centerCoord = (xg + (wg / 2), yg + (hg / 2))
+        print(centerCoord)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+
+    k = cv2.waitKey(5)
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
